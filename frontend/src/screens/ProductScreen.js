09c8687c3690ebef56a9e15c6ea8e38824c68d95
Muhammad wasi <wasiarain819@gmail.com>
2021-08-15 01:02:10 +0500

    Update ProductScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -39,6 +39,34 @@ const ProductScreen = ({ match }) => {
             </ListGroup.Item>
           </ListGroup>
         </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>
+                      ${product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button variant="btn btn-dark">Add To Cart</Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
       </Row>
     </>
   );
