cddac5da4c560a856ad6932a7a357c000ff7f33f
Muhammad wasi <wasiarain819@gmail.com>
2021-08-16 01:10:51 +0500

    change: db file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import connectDB from './data/config/db.js';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
