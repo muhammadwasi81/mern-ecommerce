87230ea6e5c946bf1f66b148da575e241c25027b
Muhammad wasi <wasiarain819@gmail.com>
2021-08-16 02:26:49 +0500

    create: product Routes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js'; // => .js for es modules
 
 dotenv.config();
 
@@ -14,14 +14,7 @@ app.get('/', (req, res) => {
   res.send('API IS RUNNING....');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
