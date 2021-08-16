7ef98c981f65a80f6ff9901d330cecfeba6c2174
Muhammad wasi <wasiarain819@gmail.com>
2021-08-16 14:01:47 +0500

    create: constants folder to format work

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,10 +1,16 @@
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from '../constants/productConstants';
+
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
-    case 'PRODUCT_LIST-REQUEST':
+    case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] };
-    case 'PRODUCT_LIST_SUCCESS':
+    case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
-    case 'PRODUCT_LIST_FAIL':
+    case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
