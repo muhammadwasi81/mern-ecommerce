14c573d81a3705c73fe87df9f11e57fcd478b3a9
Muhammad wasi <wasiarain819@gmail.com>
2021-08-18 00:33:04 +0500

    create: Register User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,6 +23,40 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400); // => 400 is a bad requests
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      // => status 201 means something is created
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
 // @desc Get user profile
 // @route POST /api/users/profile
 // @access Private
@@ -42,4 +76,5 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('Invalid email and password');
   }
 });
-export { authUser, getUserProfile };
+
+export { authUser, getUserProfile, registerUser };
