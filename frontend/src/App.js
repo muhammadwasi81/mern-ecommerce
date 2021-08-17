009331b1ee915f10c5c38e896b22bb1ab8eb64ef
Muhammad wasi <wasiarain819@gmail.com>
2021-08-18 01:42:53 +0500

    create: loginScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Container>
           <Route path="/" component={HomeScreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/login" component={LoginScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
