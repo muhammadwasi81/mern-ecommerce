238ee9a3a091ab78ca845ad9c0db76ceb5d0ca0b
Muhammad wasi <wasiarain819@gmail.com>
2021-08-18 01:03:16 +0500

    create: userActions && update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,19 +6,26 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers.js';
 import { cartReducer } from './reducers/cartReducers.js';
+import { userLoginReducer } from './reducers/userReducers.js';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLoginReducer: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItem')
   ? JSON.parse(localStorage.getItem('cartItem'))
   : [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
